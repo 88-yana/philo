b5ee940e6af77484b05ff607d427311b99ceac5a
88-yana <85784752+88-yana@users.noreply.github.com>
2023-01-11 00:06:58 +0900

    mut

--- srcs/simulate/monitor.c
+++ srcs/simulate/monitor.c
@@ -6,7 +6,7 @@
 /*   By: hyanagim <hyanagim@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 00:41:07 by hyanagim          #+#    #+#             */
-/*   Updated: 2023/01/09 22:40:42 by hyanagim         ###   ########.fr       */
+/*   Updated: 2023/01/10 23:56:08 by hyanagim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,19 @@ static bool	find_death(t_vars *vars, t_philo *philo)
 	if (vars->stop)
 		return (true);
 	(void) philo;
-	timestamp = get_timestamp(vars->start_time);
 	pthread_mutex_lock(&philo->vars->mtx_time);
+	timestamp = get_timestamp(vars->start_time);
 	if (timestamp > vars->args.time_to_die + philo->last_eat_time)
 	{
 		pthread_mutex_unlock(&philo->vars->mtx_time);
 		if (vars->stop)
 			return (true);
 		vars->stop = true;
+		pthread_mutex_unlock(&vars->mtx_stop);
 		usleep(5000);
+		pthread_mutex_lock(&vars->mtx_write);
 		printf("%d %d %s\n", timestamp, philo->id, DIED_STR);
+		pthread_mutex_unlock(&vars->mtx_write);
 		return (true);
 	}
 	pthread_mutex_unlock(&philo->vars->mtx_time);
@@ -37,21 +40,20 @@ static bool	find_death(t_vars *vars, t_philo *philo)
 
 static bool	death_detector(t_vars *vars)
 {
-	int		i;
-	bool	go_on;
+	int	i;
 
-	go_on = true;
 	i = 0;
 	while (i < vars->args.num_of_philos)
 	{
-		lock_mutex(NULL, vars, NONE);
-		if (vars->stop || find_death(vars, &vars->philos[i]))
+		pthread_mutex_lock(&vars->mtx_stop);
+		if (vars->stop)
 		{
-			go_on = false;
-			unlock_mutex(NULL, vars, NONE);
+			pthread_mutex_unlock(&vars->mtx_stop);
 			return (false);
 		}
-		unlock_mutex(NULL, vars, NONE);
+		else if (find_death(vars, &vars->philos[i]))
+			return (false);
+		pthread_mutex_unlock(&vars->mtx_stop);
 		i++;
 	}
 	return (true);
